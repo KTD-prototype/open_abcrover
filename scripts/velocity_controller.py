805a40cdf42a84db73c388d7c6cf14a06d29d506
KTD-prototype <ktd.prototype@gmail.com>
2020-04-08 00:02:19 +0900

    cleaned

--- scripts/velocity_controller.py
+++ scripts/velocity_controller.py
@@ -1,8 +1,8 @@
-# node for velocity control based on wheel odometry data and send velocity command to arduino_interface node
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+# node for velocity control based on wheel odometry data and send velocity command to arduino_interface node
 import rospy
 import signal
 import time
